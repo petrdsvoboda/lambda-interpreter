217218f1b4069e6db54da89b71c3ef51c88edb1c
Petr D. Svoboda <petrdsvoboda@gmail.com>
2020-05-24 20:39:33 +0200

    Impl Semigroup and Monoit for Term

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -10,12 +10,24 @@ import           TextParser
 
 macros :: String -> String
 macros m = case m of
-    "+" -> "(\\x.(\\y.(\\s.(\\z.(x s (y s z))))))"
-    "*" -> "(\\x.(\\y.(\\s.(x (y s)))))"
-    "0" -> "(\\s.(\\z.z))"
-    "1" -> "(\\s.(\\z.(s z)))"
-    "2" -> "(\\s.(\\z.(s (s z))))"
-    "3" -> "(\\s.(\\z.(s (s (s z)))))"
+    "SUCC"  -> "(\\x.(\\s.(\\z.(s (x s z)))))"
+    "PRED"  -> "(\\x.(\\s.(\\z.(x (\\f.(\\g.(g (f s)) (\\g.(z)) (\\m.(m))))))))"
+    "TRUE"  -> "(\\t.(\\f.(t)))"
+    "FALSE" -> "(\\t.(\\f.(f)))"
+    "AND"   -> "(\\x.(\\y.(x y x)))"
+    "OR"    -> "(\\x.(\\y.(x x y)))"
+    "NOT"   -> "(\\p.(p TRUE FALSE))"
+    "+"     -> "(\\x.(\\y.(\\s.(\\z.(x s (y s z))))))"
+    "-"     -> "(\\m.(\\n.((n PRED) m)))"
+    "*"     -> "(\\x.(\\y.(\\s.(x (y s)))))"
+    "0"     -> "(\\s.(\\z.z))"
+    "1"     -> "(\\s.(\\z.(s z)))"
+    "2"     -> "(\\s.(\\z.(s (s z))))"
+    "3"     -> "(\\s.(\\z.(s (s (s z)))))"
+    "Y"     -> "(\\f.((\\x.(f (x x))) (\\x.(f (x x)))))"
+
+replaceVar :: Char -> Term -> Term -> Term
+replaceVar c a b = 
 
 lambdaEval :: Term -> Term
 lambdaEval term = case term of
@@ -23,5 +35,7 @@ lambdaEval term = case term of
         (Simple v) -> Variable (Simple v)
         (Macro  m) -> buildExpr . getBlocks $ macros m
     (Abstraction (v, t)) -> Variable (Simple 'v')
-    (Application (a, b)) -> Variable (Simple 'v')
+    (Application (a, b)) -> case a of
+        (Abstraction (v, t)) -> replaceVar v t b
     (Normal      t     ) -> lambdaEval t
+    (Empty) -> Empty
--- src/Types.hs
+++ src/Types.hs
@@ -1,13 +1,33 @@
 module Types where
 
-data VarData = Simple Char | Macro String
-data Term = Variable VarData | Abstraction (Char, Term) | Application (Term, Term) | Normal Term
+data VarData  = Simple Char | Macro String
+data Term  = Variable VarData | Abstraction (Char, Term) | Application (Term, Term) | Normal Term | Empty
 instance Show Term where
-    show (Variable    (Simple c   )) = show c
-    show (Variable    (Macro  text)) = show text
-    show (Abstraction (c, t)       ) = show c ++ "\n\t" ++ show t
-    show (Application (a, b)       ) = show a ++ "\n" ++ show b
-    show (Normal      t            ) = show t
+    show (Variable    (Simple v   )) = show v
+    show (Variable    (Macro  text)) = text
+    show (Abstraction (v, t)       ) = "\\" ++ show v ++ "." ++ show t
+    show (Application (a, b)       ) = "(" ++ show a ++ ")(" ++ show b ++ ")"
+    show (Normal      t            ) = "(" ++ show t ++ ")"
+    show Empty                       = ""
+
+instance Semigroup Term where
+    (<>) a     Empty = a
+    (<>) Empty b     = b
+    (<>) a     b     = Application (a, b)
+
+instance Monoid Term where
+    mempty  = Empty
+    mappend = (<>)
+
+instance Foldable Term where
+    foldMap f x = case x of
+        Empty                    -> Empty
+        (Normal      t         ) -> fmap f t
+        (Application (a, b)    ) -> Application (fmap f a, fmap f b)
+        (Abstraction (v, t)    ) -> Abstraction (f v, fmap f t)
+        (Variable    (Simple v)) -> Variable (Simple (f v))
+        (Variable    (Macro  m)) -> Variable (Macro m)
+
 
 data Block = BlockText String | SubBlocks [Block]
 instance Show Block where
